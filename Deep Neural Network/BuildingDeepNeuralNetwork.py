d66c2e9a7abedf1914e7174ebd78ced605282027
homePC <1025925009@qq.com>
2022-02-23 22:14:40 +0800

    2022.2.23 home

---
+++ Deep Neural Network/BuildingDeepNeuralNetwork.py
@@ -0,0 +1,85 @@
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from testCases_v3 import *
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
+
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+np.random.seed(1)
+
+def initialize_parameters(n_x, n_h, n_y):
+    """
+    Argument:
+    n_x -- size of the input layer
+    n_h -- size of the hidden layer
+    n_y -- size of the output layer
+    
+    Returns:
+    parameters -- python dictionary containing your parameters:
+                    W1 -- weight matrix of shape (n_h, n_x)
+                    b1 -- bias vector of shape (n_h, 1)
+                    W2 -- weight matrix of shape (n_y, n_h)
+                    b2 -- bias vector of shape (n_y, 1)
+    """
+    
+    np.random.seed(1)
+    
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros((n_y, 1))
+    
+    assert(W1.shape == (n_h, n_x))
+    assert(b1.shape == (n_h, 1))
+    assert(W2.shape == (n_y, n_h))
+    assert(b2.shape == (n_y, 1))
+    
+    parameters = {"W1" : W1,
+                  "b1" : b1,
+                  "W2" : W2,
+                  "b2" : b2
+                  }
+    return parameters
+
+def initialize_parameters_deep(layer_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimensions of each layer in our network
+    
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+                    bl -- bias vector of shape (layer_dims[l], 1)
+    """
+    
+    np.random.seed(3)
+    parameters = {}
+    
+    for i in range(1, len(layer_dims)):
+        parameters['W' + str(i)] = np.random.randn(layer_dims[i],layer_dims[i-1]) * 0.01
+        parameters['b' + str(i)] = np.zeros((layer_dims[i], 1))
+        
+        assert(parameters['W' + str(i)].shape == (layer_dims[i], layer_dims[i-1]))
+        assert(parameters['b' + str(i)].shape == (layer_dims[i], 1))
+    
+    return parameters
+
+def linear_forward(A, W, b):
+    """
+    Implement the linear part of a layer's forward propagation.
+
+    Arguments:
+    A -- activations from previous layer (or input data): (size of previous layer, number of examples)
+    W -- weights matrix: numpy array of shape (size of current layer, size of previous layer)
+    b -- bias vector, numpy array of shape (size of the current layer, 1)
+
+    Returns:
+    Z -- the input of the activation function, also called pre-activation parameter 
+    cache -- a python dictionary containing "A", "W" and "b" ; stored for computing the backward pass efficiently
+    """
+    Z = np.dot()
+        
